7572af9578715b536b58cc087c2a65acbec6097d
dev.kartic <dev.kartic@gmail.com>
2020-03-16 01:02:17 +0600

    Completed ui routing with component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component{
   render() {
     return(
-        <div className="container-fluid">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">Hidden brand</a>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        </div>
+        <Router>
+            <div className="container-fluid">
+                <Navbar/>
+                <Route exact path="/"><Home/></Route>
+                <Route path="/about"><About/></Route>
+                <Route path="/contact"><Contact/></Route>
+            </div>
+        </Router>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Contact = () => {
+const Home = () => {
     return(
         <div className="card">
             <div className="card-header">
                 Featured
             </div>
             <div className="card-body">
-                <h5 className="card-title">Contact</h5>
+                <h5 className="card-title">Home</h5>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
@@ -15,4 +15,4 @@ const Contact = () => {
     )
 }
 
-export default Contact;
+export default Home;
